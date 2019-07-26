c9134ed67d0b11f4a9e8605bcbf7b376993ab1a9
ProSavage <naman0311@gmail.com>
2019-07-26 11:15:45 -0500

    Im retarded

--- routes/data.js
+++ routes/data.js
@@ -5,9 +5,8 @@ const fs = require('fs');
 /* GET users listing. */
 router.post('/', function(req, res, next) {
   const {user, html, css} = req.body;
-  fs.writeFileSync("./" + user + "/index.html", html, "utf-8");
-  fs.writeFileSync("./" + user + "/style.css", css, "utf-8");
-
+  fs.writeFileSync("./submissions/" + user + "/index.html", html, "utf-8");
+  fs.writeFileSync("./submissions/" + user + "/style.css", css, "utf-8");
 
   res.send('Submmited for ' + user);
 });
